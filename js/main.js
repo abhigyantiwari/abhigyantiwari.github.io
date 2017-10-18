e59595a5184409678939ed10057852fd3982abac
Abhigyan Tiwari <tiwari866@gmail.com>
2017-10-18 21:47:11 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $('document').ready(
         function () {
             $('.myName').hide().delay(500).fadeIn(700);
+            $('.label').bind('mouseover',animateLabel);
+            function animateLabel(){
+                $('.label').addClass('animated flash');
+            }
         }
 );
 
