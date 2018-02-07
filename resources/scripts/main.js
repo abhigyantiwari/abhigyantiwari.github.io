53a27b1e2e2cdf6da8f5f8092726b2d3e1b45417
Abhigyan <tiwari866@gmail.com>
2018-02-07 09:54:02 +0530

    added particles js

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-	
-});
+// $(document).ready(function(){
+// 	while (true) {
+// 		setTimeout(function(){
+// 			$('.nv').addClass('animated pulse');
+// 		}, 2500);
+// 		$('.nv').removeClass('animated pulse');
+// 	}
+// });
